40b640b508d264a9ae81a633b2c05f0df3a4e10a
Rudopulos Adrian Lazaros <79414851+rudopulos@users.noreply.github.com>
2026-03-30 20:57:25 +0300

    fix(router): Change basename to root for custom domain support

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import '@/index.css';
 import { Toaster } from '@/components/ui/toaster';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={import.meta.env.DEV ? '/' : '/aether-web-ro/'}>
+  <BrowserRouter basename="/">
     <App />
     <Toaster />
   </BrowserRouter>
